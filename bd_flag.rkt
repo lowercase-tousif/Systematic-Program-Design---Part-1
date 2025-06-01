6c5bcb32b081a1e609e6ce3920dbe9ecae0155a2
lowercase-tousif <toisiftasrik@gmail.com>
2025-06-02 00:30:19 +0600

    initially images added

---
+++ bd_flag.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname bd_flag) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+(define green-bg (rectangle 500 300 "solid" "darkgreen"))
+(define red-bg (circle 100 "solid" "red"))
+
+(define bd-flag (overlay red-bg green-bg))
+
+bd-flag
