0357b84262385bbeed7bfd56c07f59a4b0098c1e
lowercase-tousif <toisiftasrik@gmail.com>
2025-06-02 09:12:34 +0600

    Function definitions p1 learned

---
+++ function_definitions.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname function_definitions) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+; Learning the function definition
+(require 2htdp/image)
+
+(define (bulb c)
+  (circle 40 "solid" c))
+
+(above (bulb "red")
+       (bulb "yellow")
+       (bulb "green"))
